51cf759dbfb1011049c6d7696f49ffdcc5932bf3
soohy <soohyun120@naver.com>
2023-03-01 10:28:02 +0900

    implementation

---
+++ study/chap4/ex4-1.py
@@ -0,0 +1,21 @@
+n = int(input())
+plans = input().split()
+
+# 시작 좌표
+x,y = 1,1
+
+# L, R, U, D 에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        if nx < 1 or ny < 1 or nx > n or ny > n:
+            continue
+        x,y = nx,ny
+
+print(x,y)
