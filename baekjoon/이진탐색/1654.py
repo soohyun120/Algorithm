95f9602db846b7206ebc85ad6622ae87cd783ab8
soohy <soohyun120@naver.com>
2023-03-15 07:53:04 +0900

    binary search

---
+++ baekjoon/이진탐색/1654.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+a = [int(input()) for _ in range(k)]
+
+start = 1
+end = max(a)
+
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in a:
+        total += x // mid
+    if total >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
