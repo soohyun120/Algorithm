1d7034f5d82e8b4efed0d59c63c97291de585fe0
soohy <soohyun120@naver.com>
2023-03-20 08:22:53 +0900

    binary search

--- baekjoon/이진탐색/1654.py
+++ baekjoon/이진탐색/1654.py
@@ -2,19 +2,21 @@ import sys
 input = sys.stdin.readline
 
 k, n = map(int, input().split())
-a = [int(input()) for _ in range(k)]
+arr = [int(input()) for _ in range(k)]
 
 start = 1
-end = max(a)
+end = max(arr)
 
+result = 0
 while start <= end:
-    total = 0
     mid = (start + end) // 2
-    for x in a:
-        total += x // mid
-    if total >= n:
+    count = 0
+    for v in arr:
+        count += v // mid
+    if count >= n:
         start = mid + 1
+        result = mid
     else:
         end = mid - 1
 
-print(end)
+print(result)
