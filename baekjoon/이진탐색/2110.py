d70afae8fd7aa7db1c405567ef4e2bb72dd84cb8
soohy <soohyun120@naver.com>
2023-03-21 09:25:13 +0900

    binary search

--- baekjoon/이진탐색/2110.py
+++ baekjoon/이진탐색/2110.py
@@ -5,22 +5,20 @@ n, c = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 arr.sort()
 
-start = 1   # 최소 거리
-end = arr[-1] - arr[0]  # 최대 거리
+start = 1
+end = arr[-1] - arr[0]
 
-result = 0
 while start <= end:
     mid = (start + end) // 2
-    current = arr[0]
     count = 1
+    current = arr[0]
     for i in range(1, len(arr)):
         if arr[i] >= current + mid:
             count += 1
             current = arr[i]
     if count >= c:
         start = mid + 1
-        result = mid
     else:
         end = mid - 1
 
-print(result)
+print(end)
