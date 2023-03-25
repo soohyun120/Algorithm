3143d6ae231b27db82ee92fe5d8176c91ee7f618
soohy <soohyun120@naver.com>
2023-03-25 22:58:24 +0900

    dynamic programing

---
+++ baekjoon/동적계획법/9461.py
@@ -0,0 +1,9 @@
+T = int(input())
+n = [int(input()) for _ in range(T)]
+
+for i in range(T):
+    d = [0] * 101
+    d[1], d[2], d[3], d[4], d[5] = 1, 1, 1, 2, 2
+    for j in range(6, n[i] + 1):
+        d[j] = d[j - 5] + d[j - 1]
+    print(d[n[i]])
