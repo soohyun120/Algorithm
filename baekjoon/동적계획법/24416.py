319af9d9694cfcb017c5b55de96d294de0e9ce36
soohy <soohyun120@naver.com>
2023-04-03 10:30:42 +0900

    dynamic programing

--- baekjoon/동적계획법/24416.py
+++ baekjoon/동적계획법/24416.py
@@ -1,20 +1,25 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+f = [0] * 41
 
-def fib_recur(n):
+count = 1
+count2 = 0
+
+def fib(n):
+    global count
     if n == 1 or n == 2:
         return 1
     else:
-        return fib_recur(n - 1) + fib_recur(n - 2)
-
-def fib_dp(n):
-    d = [0] * (n + 1)
-    d[1], d[2] = 1, 1
-    count = 0
-    for i in range(3, n + 1):
-        d[i] = d[i - 1] + d[i - 2]
         count += 1
-    return count
+        return fib(n - 1) + fib(n - 2)
 
-n = int(input())
-print(fib_recur(n), fib_dp(n))
+def fibonacci(n):
+    global count2
+    f[1], f[2] = 1, 1
+    for i in range(3, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+        count2 += 1
+    return f[n]
+
+fib(n)
+fibonacci(n)
+print(count, count2)
