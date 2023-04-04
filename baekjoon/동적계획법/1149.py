bfdd4fd7253091abcdec61b1e28f18688cd99094
soohy <soohyun120@naver.com>
2023-04-04 11:37:02 +0900

    dynamic programing

--- baekjoon/동적계획법/1149.py
+++ baekjoon/동적계획법/1149.py
@@ -3,7 +3,7 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-for i in range(1, n):
+for i in range(1, len(arr)):
     arr[i][0] = min(arr[i - 1][1], arr[i - 1][2]) + arr[i][0]
     arr[i][1] = min(arr[i - 1][0], arr[i - 1][2]) + arr[i][1]
     arr[i][2] = min(arr[i - 1][0], arr[i - 1][1]) + arr[i][2]
