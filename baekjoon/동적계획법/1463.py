d106ace9c0c9137a8acf47c0f25cf33afdcf248c
soohy <soohyun120@naver.com>
2023-04-05 09:04:49 +0900

    dynamic programing

--- baekjoon/동적계획법/1463.py
+++ baekjoon/동적계획법/1463.py
@@ -1,12 +1,11 @@
 n = int(input())
-
-d = [0] * (n + 1)
+dp = [0] * (n + 1)
 
 for i in range(2, n + 1):
-    d[i] = d[i - 1] + 1
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i // 2] + 1)
+    dp[i] = dp[i - 1] + 1
     if i % 3 == 0:
-        d[i] = min(d[i], d[i // 3] + 1)
+        dp[i] = min(dp[i // 3] + 1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i // 2] + 1, dp[i])
 
-print(d[n])
+print(dp[n])
