8df9ffaa1215b43643b1602d9f8f6542b587139a
soohy <soohyun120@naver.com>
2023-04-06 13:29:56 +0900

    dynamic programing

--- baekjoon/동적계획법/2156.py
+++ baekjoon/동적계획법/2156.py
@@ -3,10 +3,10 @@ arr = [0] * 10001
 for i in range(1, n + 1):
     arr[i] = int(input())
 
-d = [0] * 10001
-d[1] = arr[1]
-d[2] = arr[1] + arr[2]
+dp = [0] * 10001
+dp[1] = arr[1]
+dp[2] = arr[1] + arr[2]
 for i in range(3, n + 1):
-    d[i] = max(d[i - 3] + arr[i - 1] + arr[i], d[i - 2] + arr[i], d[i - 1])
+    dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i], dp[i - 1])
 
-print(d[n])
+print(dp[n])
