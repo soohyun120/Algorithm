d402522bd3614e4f48a6c0b6b7bc397aa2683bad
soohy <soohyun120@naver.com>
2023-04-06 13:49:21 +0900

    dynamic programing

--- baekjoon/동적계획법/11054.py
+++ baekjoon/동적계획법/11054.py
@@ -1,23 +1,23 @@
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [1] * n
-dp2 = [1] * n
+d1 = [1] * n
+d2 = [1] * n
 
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+            d1[i] = max(d1[i], d1[j] + 1)
 
 arr.reverse()
 
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j]:
-            dp2[i] = max(dp2[i], dp2[j] + 1)
+            d2[i] = max(d2[i], d2[j] + 1)
 
-dp2.reverse()
+d2.reverse()
 
 for i in range(n):
-    dp[i] = dp[i] + dp2[i]
+    d1[i] = d1[i] + d2[i]
 
-print(max(dp) - 1)
+print(max(d1) - 1)
