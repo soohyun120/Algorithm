d2631da3cc2767e24675dd045c7f4c8883f11762
soohy <soohyun120@naver.com>
2023-06-28 19:00:15 +0900

    Greedy

--- baekjoon/그리디/1931.py
+++ baekjoon/그리디/1931.py
@@ -1,17 +1,19 @@
-n = int(input())
-time_table = []
-for i in range(n):
-    time_table.append(tuple(map(int, input().split())))
+import sys
+input = sys.stdin.readline
 
-# 1.끝나는 시간 오름차순  2.시작 시간 오름차순
-time_table.sort(key=lambda x:(x[1], x[0]))
+N = int(input())
+arr = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    arr.append((a, b))
+
+arr.sort(key=lambda x: (x[1], x[0]))
 
 result = 1
-end_time = time_table[0][1]
-for i in range(1, n):
-    if time_table[i][0] >= end_time:
+end = arr[0][1]
+for i in range(1, N):
+    if end <= arr[i][0]:
         result += 1
-        end_time = time_table[i][1]
+        end = arr[i][1]
 
 print(result)
-
