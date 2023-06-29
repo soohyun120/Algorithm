a9707054dcb691353455824d694c7448e70bc660
soohy <soohyun120@naver.com>
2023-06-29 17:19:17 +0900

    DP

--- baekjoon/동적계획법/1463.py
+++ baekjoon/동적계획법/1463.py
@@ -6,7 +6,7 @@ for i in range(2, n + 1):
     d[i] = d[i - 1] + 1
     if i % 3 == 0:
         d[i] = min(d[i // 3] + 1, d[i])
-    elif i % 2 == 0:
+    if i % 2 == 0:
         d[i] = min(d[i // 2] + 1, d[i])
 
 print(d[n])
