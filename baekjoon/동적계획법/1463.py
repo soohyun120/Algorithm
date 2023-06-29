c9c3982db55627a5d9a8bac41aa0a0b1b4f95e27
soohy <soohyun120@naver.com>
2023-06-29 17:16:56 +0900

    DP

--- baekjoon/동적계획법/1463.py
+++ baekjoon/동적계획법/1463.py
@@ -1,11 +1,12 @@
 n = int(input())
-dp = [0] * (n + 1)
+
+d = [0] * (n + 1)
 
 for i in range(2, n + 1):
-    dp[i] = dp[i - 1] + 1
+    d[i] = d[i - 1] + 1
     if i % 3 == 0:
-        dp[i] = min(dp[i // 3] + 1, dp[i])
-    if i % 2 == 0:
-        dp[i] = min(dp[i // 2] + 1, dp[i])
+        d[i] = min(d[i // 3] + 1, d[i])
+    elif i % 2 == 0:
+        d[i] = min(d[i // 2] + 1, d[i])
 
-print(dp[n])
+print(d[n])
