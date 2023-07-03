a2d4eead6803286a37f1da349ae5723900297802
soohy <soohyun120@naver.com>
2023-07-03 15:51:18 +0900

    DP

--- baekjoon/동적계획법/1932.py
+++ baekjoon/동적계획법/1932.py
@@ -1,13 +1,15 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
 
 for i in range(1, n):
-    arr[i][0] = arr[i][0] + arr[i - 1][0]
-    for j in range(1, len(arr[i]) - 1):
-        arr[i][j] = max(arr[i - 1][j - 1], arr[i - 1][j]) + arr[i][j]
-    arr[i][len(arr[i]) - 1] = arr[i - 1][len(arr[i]) - 2] + arr[i][len(arr[i]) - 1]
+    for j in range(len(arr[i])):
+        if j == 0:
+            arr[i][j] = arr[i - 1][0] + arr[i][j]
+        elif j == len(arr[i]) - 1:
+            arr[i][j] = arr[i - 1][j - 1] + arr[i][j]
+        else:
+            arr[i][j] = max(arr[i - 1][j - 1], arr[i - 1][j]) + arr[i][j]
 
 print(max(arr[n - 1]))
