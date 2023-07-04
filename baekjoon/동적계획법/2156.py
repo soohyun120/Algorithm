d506f57d0c9b1b187fa652600860930965ad636f
soohy <soohyun120@naver.com>
2023-07-04 10:53:22 +0900

    DP

--- baekjoon/동적계획법/2156.py
+++ baekjoon/동적계획법/2156.py
@@ -1,12 +1,13 @@
 n = int(input())
 arr = [0] * 10001
-for i in range(1, n + 1):
+for i in range(n):
     arr[i] = int(input())
 
-dp = [0] * 10001
-dp[1] = arr[1]
-dp[2] = arr[1] + arr[2]
-for i in range(3, n + 1):
-    dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i], dp[i - 1])
+d = [0] * 10001
+d[0] = arr[0]
+d[1] = arr[0] + arr[1]
+d[2] = max(arr[0] + arr[2], arr[1] + arr[2], d[1])
+for i in range(3, n):
+    d[i] = max(d[i - 2] + arr[i], d[i - 3] + arr[i - 1] + arr[i], d[i - 1])
 
-print(dp[n])
+print(max(d))
