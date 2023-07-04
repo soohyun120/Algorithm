c5e8e8aaf2f2b454322311ce1b8a013fc7db47ce
soohy <soohyun120@naver.com>
2023-07-04 14:20:44 +0900

    DP

--- baekjoon/동적계획법/1149.py
+++ baekjoon/동적계획법/1149.py
@@ -1,12 +1,12 @@
-n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
+import sys
+input = sys.stdin.readline
 
-for i in range(1, len(arr)):
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
     arr[i][0] = min(arr[i - 1][1], arr[i - 1][2]) + arr[i][0]
     arr[i][1] = min(arr[i - 1][0], arr[i - 1][2]) + arr[i][1]
     arr[i][2] = min(arr[i - 1][0], arr[i - 1][1]) + arr[i][2]
 
 print(min(arr[n - 1]))
-
