74d7933a1dedbae07ee27c9021ac9676758ed29c
soohy <soohyun120@naver.com>
2023-07-05 12:37:23 +0900

    DP

--- baekjoon/동적계획법/24416.py
+++ baekjoon/동적계획법/24416.py
@@ -1,26 +1,27 @@
 n = int(input())
-f = [0] * 41
 
-count = 1
-count2 = 0
+cnt1 = 0
+cnt2 = 0
 
-def fib(n):
-    global count
+def fib1(n):
+    global cnt1
     if n == 1 or n == 2:
+        cnt1 += 1
         return 1
     else:
-        count += 1
-        return fib(n - 1) + fib(n - 2)
+        return fib1(n - 1) + fib1(n - 2)
 
-def fibonacci(n):
-    global count2
-    f[1], f[2] = 1, 1
+f = [0] * (n + 1)
+
+def fib2(n):
+    global cnt2
+    f[1] = 1
+    f[2] = 1
     for i in range(3, n + 1):
+        cnt2 += 1
         f[i] = f[i - 1] + f[i - 2]
-        count2 += 1
     return f[n]
 
-fib(n)
-fibonacci(n)
-print(count, count2)
-
+fib1(n)
+fib2(n)
+print(cnt1, cnt2)
